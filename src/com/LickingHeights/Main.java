aa3ff45c394df0316b70412491a3b0d2c99c2dea
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-03-21 13:24:44 -0400

    progress on user input for # guessing

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,11 +10,20 @@ public class Main {
         userInput = new Scanner(System.in);
         System.out.println("do you want to start?(y/n?)");
         ynResponseStringType = userInput.next();
-        while (!ynResponseStringType.equalsIgnoreCase("y") && !ynResponseStringType.equalsIgnoreCase("n")) ;
-        if (ynResponseStringType.equalsIgnoreCase("y")) System.out.println("great");
+        while (!ynResponseStringType.equalsIgnoreCase("y") && !ynResponseStringType.equalsIgnoreCase("n"))
+        if (ynResponseStringType.equalsIgnoreCase("y")) {
+            System.out.println("great");
+        }
         if (ynResponseStringType.equalsIgnoreCase("n")) {
             System.out.println("Goodbye then");
             System.exit(0);
         }
+       int pickedNumber;
+        pickedNumber = (int)(Math.random()*100);
+        userInput.nextInt();
+        if(userInput<=pickedNumber && >=){
+
+        }
+        System.out.println(pickedNumber);
     }
 }
