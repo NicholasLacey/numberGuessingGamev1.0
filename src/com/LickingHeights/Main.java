93d4c664d3f246394fafb09c6f5ba0c28dc53819
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-01 14:19:56 -0400

    Loop Progession
    patching loops

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,22 +8,45 @@ public class Main {
         String ynResponseStringType;
         Scanner userInput;
         userInput = new Scanner(System.in);
-        System.out.println("do you want to start?(y/n?)");
-        ynResponseStringType = userInput.next();
-        while (!ynResponseStringType.equalsIgnoreCase("y") && !ynResponseStringType.equalsIgnoreCase("n"))
-        if (ynResponseStringType.equalsIgnoreCase("y")) {
-            System.out.println("great");
-        }
-        if (ynResponseStringType.equalsIgnoreCase("n")) {
-            System.out.println("Goodbye then");
-            System.exit(0);
-        }
-       int pickedNumber;
-        pickedNumber = (int)(Math.random()*100);
-        userInput.nextInt();
-        if(userInput<=pickedNumber && >=){
 
+        boolean gameLoop = true;
+       /* while (gameLoop) {
+
+
+
+
+
+
+
+
+            System.out.println("do you want to play again?(y/n?)");
+            ynResponseStringType = userInput.next();
+            if (ynResponseStringType.equalsIgnoreCase("y")) {
+                System.out.println("great");
+
+            }
+            else if (ynResponseStringType.equalsIgnoreCase("n")) {
+                System.out.println("Goodbye then");
+                gameLoop = false;
+                //System.exit(0);
+            }
+
+        }*/
+
+       int pickedNumber, uiNumber;
+        pickedNumber = (int)(Math.random()*100);
+        System.out.println(pickedNumber);
+        {uiNumber=userInput.nextInt();
+        if(pickedNumber>uiNumber){
+            System.out.println("Higher");
         }
+        if(pickedNumber<uiNumber){
+            System.out.println("Lower");
+        }
+        if(pickedNumber==uiNumber){
+            System.out.println("Perfect!");
+            System.exit(0);
+        }}
         System.out.println(pickedNumber);
     }
 }
