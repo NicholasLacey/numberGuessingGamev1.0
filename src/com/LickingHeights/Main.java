b6626c4173b89cffa72a00957c6ab678f00e8862
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-02 14:08:01 -0400

    Main Game finished
    finished the number guessing game working on asking if the player would like to play again

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,17 +8,10 @@ public class Main {
         String ynResponseStringType;
         Scanner userInput;
         userInput = new Scanner(System.in);
-
+        int guessTries =4;
+        int pickedNumber, uiNumber;
         boolean gameLoop = true;
-       /* while (gameLoop) {
-
-
-
-
-
-
-
-
+        while (gameLoop) {
             System.out.println("do you want to play again?(y/n?)");
             ynResponseStringType = userInput.next();
             if (ynResponseStringType.equalsIgnoreCase("y")) {
@@ -31,12 +24,11 @@ public class Main {
                 //System.exit(0);
             }
 
-        }*/
+        
 
-       int pickedNumber, uiNumber;
-        pickedNumber = (int)(Math.random()*100);
-        System.out.println(pickedNumber);
-        {uiNumber=userInput.nextInt();
+        /*pickedNumber = (int)(Math.random()*100);
+        //System.out.println(pickedNumber);
+        while (guessTries>-1){uiNumber=userInput.nextInt();
         if(pickedNumber>uiNumber){
             System.out.println("Higher");
         }
@@ -46,7 +38,10 @@ public class Main {
         if(pickedNumber==uiNumber){
             System.out.println("Perfect!");
             System.exit(0);
-        }}
-        System.out.println(pickedNumber);
+        }if (guessTries == 0) {System.out.println("\n\n\n\n\n\n\n\n\n\nyou have ran out of guesses,\nbetter luck next time!\nthe number was "+pickedNumber);}
+        if (guessTries > 0){System.out.println("you have "+guessTries+" more guess(es) left");}
+        guessTries=guessTries-1;*/
+        }
+
     }
 }
