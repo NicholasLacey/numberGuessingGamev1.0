e50f32226ca885e70e2065e797441e7eff17cf5a
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-04-05 14:01:36 -0400

    minor patches
    finished

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,33 +8,48 @@ public class Main {
         String ynResponseStringType;
         Scanner userInput;
         userInput = new Scanner(System.in);
-        int guessTries =4;
         int pickedNumber, uiNumber;
         boolean gameLoop = true;
-        while (gameLoop) {
-            System.out.println("do you want to play again?(y/n?)");
-            ynResponseStringType = userInput.next();
-            if (ynResponseStringType.equalsIgnoreCase("y")) {
+                while (gameLoop) {
+                    System.out.println("do you want to play again?(y/n?)");
+                    ynResponseStringType = userInput.next();
+                    if (ynResponseStringType.equalsIgnoreCase("y")) {
+
+
                 System.out.println("great");
-                pickedNumber = (int)(Math.random()*100);
-                System.out.println("guess a number between 1 and 100");
-                while (guessTries>-1){uiNumber=userInput.nextInt();
-                    if(pickedNumber>uiNumber){
-                        System.out.println("Higher");
+                pickedNumber = (int) (Math.random() * 100);
+                System.out.println("___" +
+                        "_____________________________________________________________________________________________" +
+                        "______" +
+                        "________________"+
+                        "\nguess a number between 1 and 100");
+                int guessTries = 5;
+                while (guessTries > -1) {
+                    uiNumber = userInput.nextInt();
+                    if (pickedNumber > uiNumber) {
+                        System.out.println("Higher, try another number");
                     }
-                    if(pickedNumber<uiNumber){
-                        System.out.println("Lower");
+                    if (pickedNumber < uiNumber) {
+                        System.out.println("Lower, try another number");
                     }
-                    if(pickedNumber==uiNumber){
-                        System.out.println("Perfect!");
+                    if (pickedNumber == uiNumber) {
+                        System.out.println("Perfect!"+"\n___" +
+                                "_____________________________________________________________________________________" +
+                                "___________________" +
+                                "___________");
                         guessTries = -1;
-                    }if (guessTries == 0) {System.out.println("\n\n\n\n\n\n\n\n\n\nyou have ran out of guesses,\nbetter luck next time!\nthe number was "+pickedNumber);}
-                    if (guessTries > 0){System.out.println("you have "+guessTries+" more guess(es) left");}
-                    guessTries=guessTries-1;
+                    }
+                    if (guessTries == 0) {
+                        System.out.println("you have ran out of guesses,\nbetter luck next time!\nthe number was " + pickedNumber+"\n___" +
+                     "___________________________________________________________________________________________________________________");
+                    }
+                    if (guessTries > 0) {
+                        System.out.println("you have " + guessTries + " more guess(es) left");
+                    }
+                    guessTries = guessTries - 1;
                 }
 
-            }
-            else if (ynResponseStringType.equalsIgnoreCase("n")) {
+            } else if (ynResponseStringType.equalsIgnoreCase("n")) {
                 System.out.println("Goodbye then");
                 gameLoop = false;
             }
